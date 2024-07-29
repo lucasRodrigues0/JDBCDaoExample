768d563fd85752d327afcd9d2140265eb6994bf0
Lucas Rodrigues <lucasrodrigues119816@gmail.com>
2024-07-29 16:47:47 -0300

    implementação da classe department

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department dp1 = new Department(1, "Caceta");
+		
+		System.out.println(dp1);
+	}
+
+}
