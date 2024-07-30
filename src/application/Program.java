06a038c209780d6d44c425d7d51ae55388ff4b27
Lucas Rodrigues <lucasrodrigues119816@gmail.com>
2024-07-30 15:21:49 -0300

    imp dao

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,6 +12,10 @@ public class Program {
 		Department dp1 = new Department(1, "Caceta");
 		
 		System.out.println(dp1);
+		
+		Seller s1 = new Seller(1, "Bob Caceta", "bob@caceta.com", new Date(), 5000.0, dp1);
+		
+		System.out.println(s1);
 	}
 
 }
