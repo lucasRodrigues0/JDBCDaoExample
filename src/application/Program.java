f951ce835b4513bb2ee695e9587ef495273110e7
Lucas Rodrigues <lucasrodrigues119816@gmail.com>
2024-08-05 17:11:21 -0300

    implementação departmentDao

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,44 +1,66 @@
 package application;
 
-import java.sql.Date;
-import java.text.SimpleDateFormat;
 import java.util.List;
+
 import model.dao.DaoFactory;
-import model.dao.SellerDao;
+import model.dao.DepartmentDao;
 import model.entities.Department;
-import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		SellerDao sdao = DaoFactory.createSellerDao();
+//		operações com seller
 
+//		SellerDao sdao = DaoFactory.createSellerDao();
+//
 //		Seller seller = sdao.findById(6);
 //		System.out.println(seller);
-
-		Department dep = new Department(3, null);
-
-		Seller newSeller = new Seller(null, "Lester Green", "lester@gmail.com", new java.util.Date(), 4000.00, dep);
+//
+//		Department dep = new Department(3, null);
+//
+//		Seller newSeller = new Seller(null, "Lester Green", "lester@gmail.com", new java.util.Date(), 4000.00, dep);
 //		sdao.insert(newSeller);
 //		System.out.println(newSeller.getId());
-
+//
 //		newSeller = sdao.findById(2);
 //
 //		System.out.println("seller: " + newSeller);
-//		
+//
 //		newSeller.setName("Lester Green");
 //		newSeller.setEmail("lester@gmail.com");
-//////		
-//////		sdao.update(newSeller);
-//////		
-//////		System.out.println("Updated Seller: " + sdao.findById(newSeller.getId()));
-////		
-//////		sdao.deleteById(2);
+//
+//		sdao.update(newSeller);
+//
+//		System.out.println("Updated Seller: " + sdao.findById(newSeller.getId()));
+//
+//		sdao.deleteById(2);
+//
+//		List<Seller> list = sdao.findAll();
+//
+//		list.forEach(System.out::println);
 
-		List<Seller> list = sdao.findAll();
+//		operações com department
 
-		list.forEach(System.out::println);
+		DepartmentDao dDao = DaoFactory.createDepartmentDao();
+
+//		Department dep = dDao.findById(4);
+//		
+//		System.out.println(dep);
+
+//		dDao.insert(new Department(null, "Donalds"));
+
+//		dDao.deleteById(6);
+		
+		Department dep = dDao.findById(3);
+		dep.setName("Food");
+		
+		dDao.update(dep);
+		
+		List<Department> depList = dDao.findAll();
+
+		depList.forEach(System.out::println);
+
 	}
 
 }
